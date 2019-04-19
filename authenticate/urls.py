10e0b8e01ef6ea4dc614f16926af9c4a30152aef
Burkay <burkay28@hotmail.de>
2019-04-20 00:11:43 +0200

    vbhjvbn

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.home,name="home"),
+   path('login/', views.login_user, name = "login_user"),
+   path('home/', views.home, name ="home"),
 ]
