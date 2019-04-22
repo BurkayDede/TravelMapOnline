ccf9deeac4d65a05cc1f6bbf7f6f94b8a18fbedd
Burkay <burkay28@hotmail.de>
2019-04-22 20:50:44 +0200

    Login und Logout

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 def home(request):
     return render(request, 'authenticate/home.html',{})
 
@@ -7,11 +8,15 @@ def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            return redirect('login')
+            return redirect('login_user')
     else:
         return render(request, 'authenticate/login.html',{})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
